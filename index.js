2b14f54327018f39cdf4a32c5ee4c8a72cb82730
Zulaikha <zhanif02@gmail.com>
2024-02-27 18:59:10 +0000

    click fixed

---
+++ index.js
@@ -0,0 +1,14 @@
+//template_3y4odfd
+//service_b202oir
+/*<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+</script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init({
+        publicKey: "EHDaYKGnJG1wc9pg6",
+      });
+   })();
+</script>
+
+*/
