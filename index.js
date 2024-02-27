6e1acfae9b2a32ad51630feb3ef4827228e8bb83
Zulaikha <zhanif02@gmail.com>
2024-02-27 19:39:26 +0000

    contact js complete

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 //template_3y4odfd
 //service_b202oir
-/*<script type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
-</script>
-<script type="text/javascript">
-   (function(){
-      emailjs.init({
-        publicKey: "EHDaYKGnJG1wc9pg6",
-      });
-   })();
-</script>
+//publicKey: "EHDaYKGnJG1wc9pg6
 
-*/
+function contact(){
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+    .sendForm(
+        'service_b202oir', 
+        'template_3y4odfd',
+        event.target,
+        'EHDaYKGnJG1wc9pg6'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        );
+    })
+}
