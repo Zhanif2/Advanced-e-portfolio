84fc7fcad0c9d593d4582745529f2f01436dc9d2
Zulaikha <zhanif02@gmail.com>
2024-03-05 19:04:35 +0000

    shapes done

--- index.js
+++ index.js
@@ -3,6 +3,21 @@
 //publicKey: "EHDaYKGnJG1wc9pg6
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+  
+    for (let i = 0; i < shapes.length; ++i) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      // Added rotate after tutorial
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+    }
+  }
+
 
 function toggleContrast(){
 contrastToggle = !contrastToggle;
