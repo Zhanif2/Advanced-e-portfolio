26f168f8ba11fdb5cdee2b16d065229e1dbf4004
z h <zhanif02@gmail.com>
2025-01-07 20:47:10 +0000

    email error

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function contact(event){
     }).catch(() => {
         loading.classList.remove("modal__overlay--visible");
         alert(
-            "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+            "The email service is temporarily unavailable. Please contact me directly on zhanif02@gmail.com"
         );
     })
 }
